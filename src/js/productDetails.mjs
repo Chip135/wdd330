7d5f5f3173935a0ca38610412ca6c6fea47a49fb
00codester <00codester@gmail.com>
2023-05-04 19:37:10 -0600

    made cart do stuff multiple times

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,16 +1,19 @@
 import { findProductById } from "./productData.mjs";
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
+let gproduct = {};
 export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-  let product = await findProductById(productId);
+  let  product = await findProductById(productId);
+  gproduct = product;
   // once we have the product details we can render out the HTML
   renderProductDetails(product);
   // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", addToCart(product));
+  document.getElementById("addToCart").addEventListener("click", addToCart);
   console.log(`Add to cart was clicked`);
 }
-function addToCart(product) {
+function addToCart() {
+    //let cartArray = [];
   let cartArray = getLocalStorage("so-cart");
   console.log(`cartArray: ${cartArray}`);
   if (!cartArray) {
@@ -18,7 +21,7 @@ function addToCart(product) {
     cartArray = [];
     setLocalStorage("so-cart", cartArray);
   }
-  cartArray.push(product);
+  cartArray.push(gproduct);
   console.log(`push cartArray: ${cartArray}`);
   setLocalStorage("so-cart", cartArray);
 }
