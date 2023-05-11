9334be3989b4d5a5f4993d6cc562316bbc1579b4
Chip135 <ethandchipman@gmail.com>
2023-05-11 16:22:32 -0600

    formatted

--- src/js/superscript.js
+++ src/js/superscript.js
@@ -4,8 +4,5 @@ export function cartCount() {
   const cartItems = getLocalStorage("so-cart");
   document.getElementById("count").innerHTML = cartItems.length;
 }
-
-
-cartCount();
 //document.getElementById("addToCart").addEventListener("click", cartCount);
-
+cartCount();
