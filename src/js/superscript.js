8c882e90d488b7b487b2370aba93755009d883f3
Chip135 <ethandchipman@gmail.com>
2023-05-12 16:19:22 -0600

    superscript works

--- src/js/superscript.js
+++ src/js/superscript.js
@@ -2,14 +2,11 @@ import { getLocalStorage } from "./utils.mjs";
 
 export function cartCount() {
   const cartItems = getLocalStorage("so-cart");
-  document.getElementById("count").innerHTML = cartItems.length;
-}
-//document.getElementById("addToCart").addEventListener("click", cartCount);
-export function runCartCount() {
-  const cartItems = getLocalStorage("so-cart").length;
-  if (cartItems >= 1) {
-    cartCount(cartItems);
+  if (!cartItems) {
+    document.getElementById("count").innerHTML = 0;
+  } else {
+    document.getElementById("count").innerHTML = cartItems.length;
   }
 }
 
-runCartCount();
+cartCount();
