3ff5a28467657187149fa650ff3318a7fb8a7463
KobeLemon <kobe42lemon@outlook.com>
2023-05-25 23:22:25 -0600

    Fixed a couple glitches with cart index.html

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,5 @@
-import { loadHeaderFooter } from "./utils.mjs";
 import shoppingCart from "./shoppingCart.mjs";
 import { cartCount } from "./superscript.mjs";
 
-loadHeaderFooter();
 shoppingCart();
 setTimeout(cartCount, 250);
