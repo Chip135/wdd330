42ec6eb13d43cc85a3c3685a8aa6cf8b8aa1da0f
KobeLemon <kobe42lemon@outlook.com>
2023-05-25 19:03:34 -0600

    Added shake to button

--- src/js/shakecart.mjs
+++ src/js/shakecart.mjs
@@ -2,11 +2,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
     shakeCart();
 });
 
-
 export function shakeCart(){
-
-
-
 
 const addCart = document.getElementById("addToCart");
 //console.log(addCart);
@@ -15,13 +11,17 @@ addCart.addEventListener("click" , (e) => {
     e.preventDefault();
     const cartIcon = document.getElementById("cartIcon");
     cartIcon.classList.add("apply-shake");
+    addCart.classList.add("apply-shake");
     endShake();
 });
 }
 
 function endShake(){
+    const addCart = document.getElementById("addToCart");
+    const cartIcon = document.getElementById("cartIcon");
     cartIcon.addEventListener("animationend", (e) => {
         cartIcon.classList.remove("apply-shake");
+        addCart.classList.remove("apply-shake");
     });
 }
 
